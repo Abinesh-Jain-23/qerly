32cc64df1816321179c01ca13e93eb545d764a1c
BabishaV <babisha@agilecybersolutions.com>
2024-01-18 15:11:16 +0530

    translation

--- lib/pages/email_page/email_page.view.dart
+++ lib/pages/email_page/email_page.view.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
-import 'package:qerly/constants/app.strings.dart';
 import 'package:qerly/pages/email_page/email_page.controller.dart';
 
 class EmailPageView extends GetResponsiveView<EmailPageController> {
   EmailPageView({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text(AppStrings.email)),
-      body: const Center(
+    return const Scaffold(
+      body: Center(
         child: TextField(),
       ),
     );
--- lib/translations/translation.dart
+++ lib/translations/translation.dart
@@ -2,16 +2,113 @@ import 'package:get/get.dart';
 
 class LocalString extends Translations {
   final Map<String, String> english = {
-    'welcome': 'Welcome',
-    'email': 'Email',
+    'welcome': "Welcome",
+    'email': "Email",
     'question1':
-        'I distinguish my capacity to recognise factors beyond my control from my ability to intellectually pinpoint the reasons for such limitations, understanding that true acceptance hinges on cognitive comprehension rather than emotional reactions.',
+        "I distinguish my capacity to recognise factors beyond my control from my ability to intellectually pinpoint the reasons for such limitations, understanding that true acceptance hinges on cognitive comprehension rather than emotional reactions.",
+    'question2':
+        "I closely assess my drive to embrace challenges as avenues for growth, my ability to manage my fear of failure, and my consistent commitment to self-care, recognising that true ambition balances aspiration with well-being to fortify self-confidence and mental resilience.",
+    'question3':
+        "I have the agency and authority to make and act on my thoughts and decisions, undeterred by competing personal circumstances, external pressures or societal expectations.",
+    'question4':
+        "I keenly observe my emotional resilience and comfort levels when faced with volatility, uncertainty, complexity, and ambiguity, understanding that true awareness lies in my response to these challenges.",
+    'question5':
+        "I probe my capacity for introspective thought, my objective open-mindedness, and my diligent nurturing of critical and unbiased thinking, understanding that true discernment is an art of mindful reflection and unbiased evaluation.",
+    'question6':
+        "I meticulously examine my personal purpose for entrepreneurship and its alignment with my business vision, understanding that genuine intention weaves a cohesive thread between personal aspiration and organisational mission.",
+    'question7':
+        "I possess a genuine and deeply-rooted belief in my business idea, an intense enthusiasm for the impact I can create, and an unwavering drive to deliver meaningful solutions to the problems I aim to solve.",
+    'question8':
+        "My entrepreneurial pursuit goes beyond personal success or profit, but is rooted in a profound sense of purpose that embodies my deeper motivations for 'why'I want to do this.",
+    'question9':
+        "I scrutinise my dedication to cultivating relationships with those who bolster my aspirations, empathise with my concerns, and guide me to fine-tuning purpose-driven actions, recognising that true refinement emerges from valuable interpersonal connections.",
+    'question10':
+        "I swiftly adapt and recover from challenges and setbacks, am not discouraged by adversity, and actively use difficulty as a learning opportunity for personal growth and motivation to continue working toward my goals.",
+    'question11':
+        "I assess my commitment to owning my reactions to uncontrollable circumstances and my duty to harness my utmost potential as an entrepreneur, recognising that true responsibility is rooted in both accountability and the drive to achieve one's highest purpose.",
+    'question12':
+        "In your opinion, please rank the following 11 mindset disciplines (with 1 being the most valuable and 11 being the least valuable) for your own entrepreneurial journey.",
+    'skill1': "Acceptance",
+    'skill2': "Ambition",
+    'skill3': "Autonomy",
+    'skill4': "Awareness",
+    'skill5': "Discernment",
+    'skill6': "Intention",
+    'skill7': "Passion",
+    'skill8': "Purpose",
+    'skill9': "Refinement",
+    'skill10': "Resilience",
+    'skill11': "Responsibility",
+    'ranking':"Mindset Ranking",
+    'important1':"Not a Priority",
+    'important2':"Low Priority",
+    'important3':"Priority",
+    'important4':"High Priority",
+    'important5':"Critical",
+    'performance1':"I am terrible at this",
+    'performance2':"I am poor at this",
+    'performance3':"I am average at this",
+    'performance4':"I am good at this",
+    'performance5':"I am excellent at this",
+    'importantqn':"How important is it that I achieve this?",
+    'performanceqn':"How do I rate my performance against this?",
+    'next':"Next",
+    "previous":"Previous",
   };
   final Map<String, String> japanese = {
-    'welcome': 'いらっしゃいませ',
-    'email': 'Eメール',
+    'welcome': "いらっしゃいませ",
+    'email': "Eメール",
     'question1':
-        '私はコントロールできない要因を見分ける能力と、理知的に制限の理由を突き止める能力を区別し、「真の受容性」とは感情的な反応ではなく、認知的な理解力によるものだとわかっている。'
+        "私はコントロールできない要因を見分ける能力と、理知的に制限の理由を突き止める能力を区別し、「真の受容性」とは感情的な反応ではなく、認知的な理解力によるものだとわかっている。",
+    'question2':
+        "私は、成長のための挑戦を受け入れる意欲、失敗を恐れる気持ちをコントロールする能力、そしてセルフケアへの一貫した取り組みを綿密に評価し、「真の向上心」とは、願望とウェルビーイングのバランスをとりながら、自信と精神的回復力(メンタル・レジリエンス)を高めるものだと理解している。",
+    'question3':
+        "私は自分の考えや 決断を下し、それに基づいて行動する主体性と権限を持っており、個人的な状況、外部からのプレッシャー、社会的な期待に惑わされることなく行動します。",
+    'question4':
+        "私は、変動性、不確実性、複雑性、曖昧性に直面した時、自分の感情的な弾力性（エモーショナル・レジリエンス）と自身の快適さのレベルを鋭く観察し、これらのチャレンジに対する自分の対応にこそ真の気づきがあることを理解している。",
+    'question5':
+        "私は、自分の内省的思考能力、客観的な自由闊達さ(オープンマインド)、真面目で偏見のない思考力を育てようと努力している。なぜなら、「真の識別力」とは、心のこもった内省と偏見のない判断力であると理解しているからである。",
+    'question6':
+        "私は、起業家としての自己目的と、それがビジネスビジョンとどのようにマッチしているかに細心の注意を払っています。なぜなら、「真なる志」とは、個人の願望と経営理念を結びつける重要な筋であると理解しています。",
+    'question7':
+        "私は自分のビジネス・アイデアに対する純粋で根強い信念、自分が生み出すことができるインパクトに対する強い熱意、そして自分が解決を目指す問題に対して意味のあるソリューションを届けたいという揺るぎない意欲を持っています。",
+    'question8':
+        "私の起業家精神は、個人的な成功や利益にとどまらず、「Why（なぜ）これをやりたいのか」という自分の心の奥底にある原動力をかたちにしようとする、使命感の深さに根ざしている。",
+    'question9':
+        "私は、自分の願望を支え、悩みを共有し、自分の意志ある行動を高めるよう導いてくれる人々との関係を築くことに尽くしてきたか、自分を厳しく見つめ直します。「真の洗練さ」とは、貴重な人間関係から生まれると認識しています。",
+    'question10':
+        "私は困難や 挫折にも素早く適応して立ち直ります。逆境にめげることなく、困難を自己成長のための学びの機会として積極的に活用し、目標に向かって努力し続けるモチベーションにしています。",
+    'question11':
+        "私は、コントロール不可能な状況に対する自分の反応や 、起業家としての最大限の可能性を活用する義務を自らに課し、「真の責任感」とは、説明責任と自分の最善の目的（パーパス）を成し遂げようとする意欲の両方に根ざしていることを理解しています。",
+    'question12':
+        "以下の11項目のマインドセット要素を、ご自身の起業家精神の向上の観点からランク付けしてください（1が最も価値が高く、11が最も価値が低いとします）。",
+    'skill1': "受容性",
+    'skill2': "向上心",
+    'skill3': "自律性",
+    'skill4': "自己認識",
+    'skill5': "識別力",
+    'skill6': "志",
+    'skill7': "熱意",
+    'skill8': "使命感",
+    'skill9': "洗練",
+    'skill10': "忍耐力",
+    'skill11': "責任感",
+    'ranking':"マインドセット・ランキング",
+    'important1':"優先ではない",
+    'important2':"低い優先度",
+    'important3':"通常の優先度",
+    'important4':"高い優先度",
+    'important5':"最高の優先度",
+    'performance1':"とても苦手",
+    'performance2':"やや苦手",
+    'performance3':"どちらとも言えない",
+    'performance4':"得意",
+    'performance5':"とても得意",
+    'importantqn':"これを達成することが、私自身にとってどれほど重要ですか？",
+   'performanceqn':"これに対して自分のパフォーマンスをどのように評価しますか？",
+    'next':"次",
+    "previous":"前の",
+
   };
   @override
   Map<String, Map<String, String>> get keys => {'en': english, 'jp': japanese};
